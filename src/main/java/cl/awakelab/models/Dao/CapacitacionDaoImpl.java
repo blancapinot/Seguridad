3de318399b85b27c75e800c971f259e264c8a00f
blancapinot <bpinotj@outlook.com>
2022-10-27 21:49:30 -0300

    crea metodo delete

--- src/main/java/cl/awakelab/models/Dao/CapacitacionDaoImpl.java
+++ src/main/java/cl/awakelab/models/Dao/CapacitacionDaoImpl.java
@@ -38,7 +38,8 @@ public class CapacitacionDaoImpl implements ICapacitacionDao {
 
     @Override
     public void eliminar(int id) {
-
+    String sql = "delete capacitaciones where id_capacitacion = ?";
+    template.update (sql, new Object() {id};
     }
 }
 
