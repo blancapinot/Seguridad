e3e149f5598d60866e2b5246698d2638feea7f64
Leo <cspraword@gmail.com>
2022-10-28 21:57:52 -0300

    Se crea el Controller para capacitacion y se implementa el metodo readAll

--- src/main/java/cl/awakelab/controllers/CapacitacionController.java
+++ src/main/java/cl/awakelab/controllers/CapacitacionController.java
@@ -1,9 +1,19 @@
 package cl.awakelab.controllers;
 
+import cl.awakelab.models.entities.Capacitacion;
 import cl.awakelab.models.services.CapacitacionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class CapacitacionController {
-    private CapacitacionService
+    @Autowired
+    private CapacitacionService cs;
+    public ModelAndView readAll(){
+        List<Capacitacion> listarCapacitaciones = cs.getAll();
+        return new ModelAndView("listarCapacitaciones","capacitaciones",listarCapacitaciones);
+    }
 }
